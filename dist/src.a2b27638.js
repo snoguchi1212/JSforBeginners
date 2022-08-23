9056c1dc29d7c5010aa880efcef1a3e6d3c632f1
S.Noguchi <skettama_19x3.09ot@icloud.com>
2022-08-23 11:39:36 +0000

    Update fe3be3f94266f7a85dbe6ee3a0bf6c.json and src.a2b27638.js

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -223,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38299" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36737" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
